3bcf7e3c3378ab66584b357f22adc66e9cdf2519
Griffin Proctor <GriffinProctor1@gmail.com>
2023-11-22 15:52:20 -0800

    wip

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -5,7 +5,7 @@ import * as yup from "yup"
 const schema = yup.object().shape({
     name: yup
     .string()
-    .min(2, 'Name must be at least 2 characters')
+    .min(2, 'name must be at least 2 characters')
     .required('Name is required')
 })
 
--- src/pizzaForm.cy.js
+++ src/pizzaForm.cy.js
@@ -2,7 +2,7 @@ describe('PizzaForm', () => {
     it('displays error message for invalid name', () => {
         cy.visit('./pizza')
         cy.get('#order-button').click()
-        cy.contains('Name must be at least 2 characters').should('exist')
+        cy.contains('name must be at least 2 characters').should('exist')
 
     })
 })
